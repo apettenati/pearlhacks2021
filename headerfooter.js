5c1cde7d678b60547361344a1e4112267122b4fc
Amanda Pettenati <amanda@pette.net>
2021-02-20 21:04:21 -0500

    center navbar text

--- headerfooter.js
+++ headerfooter.js
@@ -5,7 +5,7 @@ window.onload = function () {
     header.innerHTML = ` 
       <header>
         <nav class="navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
+          <div class="container">
             <a class="navbar-brand" href="index.html">Personal Finance Tracker</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
