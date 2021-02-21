955c6f67409186831cdc96a1b9f1a70cb4e541cc
Amanda Pettenati <amanda@pette.net>
2021-02-21 08:38:47 -0500

    add padding to header and shift buttons to right

--- headerfooter.js
+++ headerfooter.js
@@ -4,23 +4,20 @@ window.onload = function () {
 
     header.innerHTML = ` 
       <header>
-        <nav class="fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <nav class="fixed-top navbar-expand navbar-dark bg-dark">
           <div class="header">
-            <a class="navbar-brand" href="index.html">"Pearl"-sonal Finance Hacker</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <a class="navbar-brand" style="padding-left: 20%;" href="index.html">"Pearl"-sonal Finance Hacker</a>
+            <div class="navbar-right" style="padding-right: 20%;" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link" href="login.html">Log In</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="register.html">Register</a>
-                </li>
-                <li class="nav-item">
+                <li class="nav-item mx-2">
                   <a class="nav-link" href="faq.html">FAQ</a>
                 </li>
+                <li class="nav-item mx-2">
+                  <a class="nav-link" href="login.html">Log In</a>
+                </li>
+                <li class="nav-item mx-2">
+                  <a class="nav-link" href="register.html">Register</a>
               </ul>
             </div>
           </div>
