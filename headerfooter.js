98d595801d2e1d16f97bdc67b74d1eabd60d99a8
Amanda Pettenati <amanda@pette.net>
2021-02-21 08:14:43 -0500

    update title

--- headerfooter.js
+++ headerfooter.js
@@ -6,7 +6,7 @@ window.onload = function () {
       <header>
         <nav class="navbar-expand-lg navbar-dark bg-dark">
           <div class="header">
-            <a class="navbar-brand" href="index.html">Personal Finance Tracker</a>
+            <a class="navbar-brand" href="index.html">"Pearl"-sonal Finance Hacker</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
