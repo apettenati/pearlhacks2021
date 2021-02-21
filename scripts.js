f2db75b16148363b4961e14cedc7596a9c7e7513
Goh, Ema <egoh9@gatech.edu>
2021-02-20 22:50:12 -0500

    edit getdata

--- scripts.js
+++ scripts.js
@@ -36,19 +36,6 @@ function skipTask(event) {
 } 
 
 function saveText(event) {
-    // const button = event.target;
-    // const card = button.parentElement;
-    // listHeader = card.getElementById("list-header");
-    // listHeader.style.display = "block";
-    // const cardChildren = button.parentElement.children;
-
-    // var list = card.getElementById("commlist");
-    // // var textVal = document.getElementById("text").value;
-    // var text = card.createElement('li');
-    // text.appendChild(card.createTextNode(textVal));
-    // list.appendChild(text);
-    // // card.appendChild(text);
-    // // card.appendChild(blank);
     const button = event.target;
     const card = button.parentElement;
     const cardChildren = button.parentElement.children;
@@ -70,6 +57,7 @@ function saveText(event) {
 
 function setData(data) {
     let completed;
+    console.log("local storage: ", localStorage.getItem('completed'))
     if (localStorage.getItem('completed') === null) {
         completed = [];
     }
@@ -83,15 +71,54 @@ function setData(data) {
         completed.push(data);
     }
     localStorage.setItem('completed', JSON.stringify(completed));
+    console.log("completed end of setdata: ", completed)
 }
 
 function getData() {
     // Check if there's data saved
+    // get array of titles of the cards (20)
+    // iterate thru array find the ones that are in completed (indexOf)
+    // cardChildren[1].classList.toggle("completed-strikethrough");
+
     let completed;
+    var titles = document.getElementsByClassName("card-title")
+    console.log("title: ", titles)
+    console.log("local storage length: ", localStorage.getItem('completed').length)
+    console.log("local storage: ", typeof(localStorage.getItem('completed')))
+    var arr = JSON.parse(localStorage.getItem('completed'))
+    console.log("arr: ", arr)
+    var i
+    var j
+    for (i = 0; i < titles.length; i++) {
+        for (j = 0; j < arr.length; j++) {
+            if (titles[i].innerHTML == arr[j]) {
+                console.log("title: ", titles[i].innerHTML)
+                var parent = titles[i].parentElement.parentElement
+                parent.children[1].classList.value = ("completed")
+                console.log("completed: ", document.getElementsByClassName("completed"))
+                console.log("parent: ", parent)
+                var child = parent.children
+                console.log("child: ", child)
+                child[1].children[0].classList.value = "completed-strikethrough"
+                child[1].children[1].classList.value = "completed-strikethrough"
+                console.log("strikethrough: ", document.getElementsByClassName("completed-strikethrough"))
+            }
+        }
+    }
+    console.log("getdata: ", localStorage.getItem('completed'))
     if(localStorage.getItem('completed') === null) {
         completed = [];
     }
     else {
+
         completed = JSON.parse(localStorage.getItem('completed'));
     }
 }
+
+// event listener for when page reloads
+// store data thats currenrtlt changed
+// send data w event listener 
+
+window.addEventListener('load', event => {
+    getData()
+})
